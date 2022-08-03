868ed2b648d4058a91711519d8395cbda8bba3ef
phm940916@gmail.com <phm940916@gmail.com>
2022-08-03 22:32:31 +0900

    config server yml 설정 eureka server yml 설정 main service yml 설정 및 git-configuration 설정(DB, eureka, port 등)

---
+++ config-server/src/main/java/com/msa/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.msa.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@EnableConfigServer
+@Slf4j
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+		log.info("-------------config-server on------------");
+	}
+
+}
---
+++ main-service/src/main/java/com/msa/mainservice/MainServiceApplication.java
@@ -0,0 +1,19 @@
+package com.msa.mainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@EnableEurekaClient
+@Slf4j
+public class MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceApplication.class, args);
+		log.info("---------------- Main Service On ----------------");
+	}
+
+}
